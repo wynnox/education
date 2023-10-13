1c40ff5e107639d7300739856d54d6c3a866948a
wynnox <alenakulikova248@gmail.com>
2023-10-14 02:06:10 +0300

    done xor32!!!!!!!!

--- sOSI/lab1/task4/functions.c
+++ sOSI/lab1/task4/functions.c
@@ -2,6 +2,7 @@
 
 enum errors xor8_file(FILE* input, unsigned int * result)
 {
+    *result = 0;
     unsigned char c;
     while(fread(&c, sizeof(unsigned char), sizeof(c), input))
     {
@@ -16,15 +17,34 @@ enum errors xor8_file(FILE* input, unsigned int * result)
 
 enum errors xor32_file(FILE* input, unsigned int * result)
 {
-    unsigned char c;
-    while(fread(&c, sizeof(unsigned char), 1, input) > 0)
+    *result = 0;
+    size_t size_group = 4;
+    unsigned char* group = (unsigned char *)malloc(sizeof(unsigned char) * size_group);
+    if(group == NULL)
     {
-        *result ^= c;
+        return INVALID_MEMORY;
     }
-
+    for(size_t i = 0; i < size_group; ++i)
+    {
+        group[i] = 0;
+    }
+    int count_read;
+    while( (count_read = fread(group, sizeof(unsigned char), size_group, input)) > 0)
+    {
+        if(count_read != 4)
+        {
+            for (size_t i = count_read; i < 4; i++)
+            {
+                group[i] = 0x00;
+            }
+        }
+        *result ^= *(unsigned int*)group;
+    }
     if(ferror(input))
     {
+        free(group);
         return ERROR_READ_FILE;
     }
+    free(group);
     return OK;
 }
