d9172bb2a419fb68a21ad4ebd16fde55d78d2685
wynnox <alenakulikova248@gmail.com>
2023-10-14 11:13:29 +0300

    done-done

--- sOSI/lab1/task3/main.c
+++ sOSI/lab1/task3/main.c
@@ -10,32 +10,50 @@ int main(int argc, char* argv[])
     FILE * input;
     FILE * output;
 
-    input = fopen(argv[1], "r");
+    input = fopen(argv[1], "rb");
     if(input == NULL)
     {
         printf("ошибка при открытии файла\n");
         return 1;
     }
-    output = fopen(argv[2], "w");
+    output = fopen(argv[2], "wb");
     if(output == NULL)
     {
         printf("ошибка при открытии файла\n");
-        if(input != NULL) fclose(input);
+        fclose(input);
         return 1;
     }
 
-    char c = fgetc(input);
-    while (!feof(input))
+    unsigned char c;
+    while (fread(&c, sizeof(unsigned char), sizeof(c), input))
     {
-        if(fputc(c, output) == EOF)
+        if(fwrite(&c,sizeof(unsigned char), sizeof(c), input) != sizeof(c))
         {
-            printf("произошла ошибка при записи байта\n");
+            printf("ошибка при записи в файл\n");
             if(input != NULL) fclose(input);
-            if(output != NULL) fclose(output);
             return 1;
         }
-        c = fgetc(input);
     }
+
+    if(ferror(input))
+    {
+        printf("ошибка при чтении из файла\n");
+        if(input != NULL) fclose(input);
+        return 2;
+    }
+
+//    char c = fgetc(input);
+//    while (!feof(input))
+//    {
+//        if(fputc(c, output) == EOF)
+//        {
+//            printf("произошла ошибка при записи байта\n");
+//            if(input != NULL) fclose(input);
+//            if(output != NULL) fclose(output);
+//            return 1;
+//        }
+//        c = fgetc(input);
+//    }
     printf("файл %s скопирован в файл %s\n", argv[1], argv[2]);
     if(input != NULL) fclose(input);
     if(output != NULL) fclose(output);
